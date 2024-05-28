b6cbf1520e9c08dfe9aa70a0d5f0891076ffcdd0
dfysa <846241197@qq.com>
2024-05-28 14:17:57 +0800

    项目初始化

--- src/main/java/com/clb/mapper/BorrowMapper.java
+++ src/main/java/com/clb/mapper/BorrowMapper.java
@@ -14,8 +14,10 @@ public interface BorrowMapper extends BaseMapper<Borrow> {
     //@Select("SELECT * FROM borrow WHERE reader_id = #{readerId}")
     //List<Borrow> selectByReaderId(Integer readerId);
 
-    @Select("SELECT * FROM borrowvo WHERE reader_id = #{readerId}")
-    List<BorrowVo> selectByReaderId(Integer readerId);
+//    @Select("SELECT * FROM borrowvo WHERE reader_id = #{readerId}")
+//    List<BorrowVo> selectByReaderId(Integer readerId);
+@Select("SELECT * FROM borrow WHERE reader_id = #{readerId}")
+List<BorrowVo> selectByReaderId(Integer readerId);
 
     void updateReturnDateById(Integer id, Date now);
 }
